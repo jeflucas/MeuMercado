519b8068760541d6b4f8135e154ed70adb9ecca3
jeflucas <jef.lucas@hotmail.co.uk>
2021-10-02 19:19:44 -0300

    CTD

---
+++ src/pages/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { View, Text, TouchableOpacity } from "react-native";
+
+const Main = () => {
+  return (
+    <View>
+      <Text>Meu Mercado</Text>
+      <TouchableOpacity>
+        <Text>+</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Main;
