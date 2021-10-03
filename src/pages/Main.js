ba9eb03075975d83300a7efd94fa0aa3325ef515
jeflucas <jef.lucas@hotmail.co.uk>
2021-10-03 14:44:37 -0300

    Update Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,29 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, FlatList } from "react-native";
 
 import Icon from "react-native-vector-icons/FontAwesome";
 
 const Main = () => {
+
+  const data = [
+    {
+      id: "1",
+      title: "Arroz",
+      price: 12.50,
+      brand: "Tio Urbano",
+      lastPrice: 11.00,
+      buy: false,
+      photo: null
+    }
+  ]
+
   return (
     <View>
       <Text>Meu Mercado</Text>
       <TouchableOpacity>
         <Icon name="cart-plus" size={30} color="#000" />
       </TouchableOpacity>
+      <FlatList data={} renderItem={} keyExtractor={} />
     </View>
   );
 };
